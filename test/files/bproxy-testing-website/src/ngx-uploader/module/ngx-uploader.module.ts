86f5845b0b1fd2a672934f12b414260647539c59
Martin Oprešnik <martinopresnik@hotmail.com>
2018-06-05 16:17:23 +0200

    test(): added bproxy-testing-website

---
+++ test/files/bproxy-testing-website/src/ngx-uploader/module/ngx-uploader.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { NgFileDropDirective } from '../directives/ng-file-drop.directive';
+import { NgFileSelectDirective } from '../directives/ng-file-select.directive';
+
+@NgModule({
+  declarations: [
+    NgFileSelectDirective,
+    NgFileDropDirective
+  ],
+  exports: [
+    NgFileSelectDirective,
+    NgFileDropDirective
+  ]
+})
+export class NgUploaderModule {}
